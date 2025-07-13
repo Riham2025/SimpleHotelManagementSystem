453c2bdd5b4052dfd13ac7300c27156822ee242d
riham <riham91.alsiyabi@gmail.com>
2025-07-13 09:48:47 +0400

    Declares a public class named Room that can be used anywhere in the project

---
+++ Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelManagementSystem
+{
+    // Represents a hotel room
+    public class Room // Declares a public class named Room that can be used anywhere in the project
+
+    {
+
+
+    }
+}
