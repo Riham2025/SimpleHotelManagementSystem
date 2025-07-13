044caa2b548be880c913a34f7c438053e9dadb97
riham <riham91.alsiyabi@gmail.com>
2025-07-13 16:19:46 +0400

    // Represents a booking between a guest and a room

---
+++ Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHotelManagementSystem
+{
+    // Represents a booking between a guest and a room
+    class Booking
+    {
+        private Room room;
+        private Guest guest;
+    }
+}
