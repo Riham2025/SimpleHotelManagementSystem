8f8adb238cf2909ea44b3a8168934b4f253b676a
riham <riham91.alsiyabi@gmail.com>
2025-07-13 09:52:51 +0400

    These are private fields — internal variables that store the room’s number and whether it is booked.

--- Room.cs
+++ Room.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace SimpleHotelManagementSystem
 {
@@ -10,9 +11,9 @@ namespace SimpleHotelManagementSystem
     public class Room // Declares a public class named Room that can be used anywhere in the project
 
     {
-        
+
         // Private fields
-        private int roomNumber;
+        private int roomNumber; //These are private fields — internal variables that store the room’s number and whether it is booked.
         private bool isBooked;
 
 
