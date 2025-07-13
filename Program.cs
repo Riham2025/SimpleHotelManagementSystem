bbe7e303014a7506bebb61dd81ea7025a9321b33
riham <riham91.alsiyabi@gmail.com>
2025-07-13 15:06:53 +0400

    Sample usage in Main():

--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@
             room1.Book();
             Console.WriteLine($"After booking - IsBooked: {room1.IsBooked}");
 
+
+            Guest guest1 = new Guest("Sara", "ID123456789");
+            guest1.Password = "StrongPassword123";  // Can set, but can't read
+            Console.WriteLine($"Guest Name: {guest1.Name}, National ID: {guest1.NationalID}");
+
+
         }
     }
 }
